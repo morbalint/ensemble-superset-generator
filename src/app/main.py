dad7d3d7185c8454f77c00c86361628315a249ac
balint <balint@morasz.hu>
2019-05-11 12:00:59 +0200

    as recieved today@11:39

--- src/app/main.py
+++ src/app/main.py
@@ -1,34 +1,54 @@
 import os
 import sys
+import numpy as np
 sys.path.append(os.getcwd()) # this is fuking ridiculus
 #import src.app.rd as rd
 import rd,load_diamides
 
+
+def create_ListFile(pwd_full_data,pwd_filename):
+    
+    list_file = os.listdir(pwd_full_data)
+    f = open( pwd+'/list_file.txt', 'w') 
+                  # A lista tartalmát egy fileba írom soronként
+    for item in list_file:
+        
+        f.write("%s\n" % item)
+        
+    f.close()
+
+
+#def join_AAs()
+
 if __name__ == '__main__':
     
-    print("hello world!")
+    
     #rd.json_test()
     #rd.TDRD_test()
     #rd.NDRD_test()
-    #print("hello world!")
+   
     pwd = os.getcwd()
     pwd_home = pwd[0:pwd.find("src")]                       #Az a mappa ami tartalmazza a project fileokat
     pwd_data = "samples/diamides"+pwd[pwd.find("src")-1]    #Az a mappa ami tartalmazza a pdb fileokat.
-    pwd_full_data = pwd_home+pwd_data                       #A pdb fileok teljes elérési útvonala, igyekeztem ezt dinamikussá tenni
-                                                            #hogy mindkettőnk gépén lefusson
+    pwd_full_data = pwd_home+pwd_data                       
+    pwd_jsons = pwd_home+"samples/"  
+    pwd_list_data = pwd+'/list_file.txt'                                             
 
-    list_file = os.listdir(pwd_full_data)                   # Egy listába írom a pdb fileneveket
-    
+    list_file = os.listdir(pwd_full_data)                  
+    create_ListFile(pwd_full_data, pwd_list_data)   # Egy listába írom a pdb fileneveket
 
+    
+    DATAbase = load_diamides.DiamidesDb(pwd_list_data,pwd_full_data)
+    
+    
+    
+    resol_num = 5 ### FIGYELEM EZ AZ ADAT BELE VAN ÉGETVE A JSON FILEBA! EZT KEZELNI KELL
+    filename1 = pwd_jsons+"NDRD_R_TCBIG_pretty.json"
+    filename2 = pwd_jsons+"TDRD_R_TCBIG.json"
+    
+    
+    AA_db = load_diamides.AAResidue_db(DATAbase,resol_num) #Itt készítem el az aminosav adatbázist
 
-    with open( pwd_full_data+'list_file.txt', 'w') as f:                   # A lista tartalmát egy fileba írom soronként
-        for item in list_file:
-            f.write("%s\n" % item)
-        f.close()
+    alma = AA_db.query(filename2,"TDRD",10,'A')  #Itt kérek egy aminosavat az adatbázisból
     
-    
-    pwd_list_data = pwd_full_data+'list_file.txt'
-    #load_diamides.DiamidesDb DATAbase
-    
-    DATAbase = load_diamides.DiamidesDb(pwd_list_data)
-    #(list_file)
+    print("hello world!")
